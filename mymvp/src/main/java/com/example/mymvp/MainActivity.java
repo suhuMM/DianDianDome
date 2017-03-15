24c81972145088742844f12499445fd58c9c44e4
suhu <suhu0824@sina.com>
2017-03-15 17:59:26 +0800

    add glide

--- mymvp/src/main/java/com/example/mymvp/MainActivity.java
+++ mymvp/src/main/java/com/example/mymvp/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.mymvp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.mymvp.api.ApiManager;
 import com.example.mymvp.api.ApiUrl;
 import com.example.mymvp.bean.Tngou;
 import com.google.gson.Gson;
 
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.CropSquareTransformation;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements Callback<String> {
     private TextView textView;
+    private ImageView imageView;
+    private String url = "http://pic4.nipic.com/20091117/3376018_110331702620_2.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,16 @@ public class MainActivity extends AppCompatActivity implements Callback<String> 
         //ApiManager.getInstance().getData(this);
         ApiManager.getInstance().tngou(0,1,20,this);
         textView = (TextView) findViewById(R.id.tv);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        //Glide.with(this).load(url).thumbnail(0.1f).skipMemoryCache(true).into(imageView);
+        Glide
+            .with(this)
+            .load("http://dev-kupao.mobifox.cn//Beauty/sportsdata/starts/2017-02-27/58b3e94c35cc6.jpg")
+            .bitmapTransform(new BlurTransformation(this,1))
+//            .bitmapTransform(new CropCircleTransformation(this))//圆形
+            .bitmapTransform(new CropSquareTransformation(this))//正方形
+            .into(imageView);
 
     }
 
